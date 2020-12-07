7fccf4087e47d3daca17c455114705faecef5f00
MaxBardyn <maxbardyn@gmail.com>
2020-12-07 14:18:28 +0200

    change string template

--- src/main/java/lab5/Algorithm.java
+++ src/main/java/lab5/Algorithm.java
@@ -7,18 +7,18 @@ public class Algorithm {
 
     static ArrayList<ArrayList<Integer>> naiveAlgorithm(String string, String substring) {
 
-        ArrayList<ArrayList<Integer>> foundArray = new ArrayList<>();
+        var foundArray = new ArrayList<ArrayList<Integer>>();
 
-        for (int currentIndex = 0; currentIndex < string.length(); currentIndex++) {
+        for (var currentIndex = 0; currentIndex < string.length(); currentIndex++) {
 
-            ArrayList<Integer> pair = new ArrayList<>();
-            int lengthOfMatches = 0;
+            var pair = new ArrayList<Integer>();
+            var lengthOfMatches = 0;
             while (lengthOfMatches < substring.length() && currentIndex + lengthOfMatches < string.length() && substring.charAt(lengthOfMatches) == string.charAt(currentIndex + lengthOfMatches)) {
                 lengthOfMatches++;
             }
 
             if (lengthOfMatches == substring.length()) {
-                int end = currentIndex + lengthOfMatches - 1;
+                var end = currentIndex + lengthOfMatches - 1;
                 pair.add(currentIndex);
                 pair.add(end);
                 foundArray.add(pair);
--- src/main/java/lab5/Main.java
+++ src/main/java/lab5/Main.java
@@ -7,8 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var string = "Six sick hicks nick six slick bricks with picks and sticks";
-        var substring = "ick";
+        var string = "I wish to wish the wish you wish to wish, but if you wish the wish the witch wishes," +
+                " I won’t wish the wish you wish to wish";
+        var substring = "wish";
         System.out.println(Arrays.toString(Algorithm.naiveAlgorithm(string, substring).toArray()));
     }
 
